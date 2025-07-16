6134f3a18c0f6ebf9c58744328d8389464ba042a
Vijaykumar-Koduru <koduru.vijaykumar.7@gmail.com>
2025-07-16 10:41:08 +0530

    created

---
+++ 2-variables.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+PERSON1=Modi
+PERSON2=Musk
+
+echo "$PERSON1:: hey $PERSON2. How is the life"
+echo "$PERSON2:: Hello $PERSON1, Nice to see you"
+echo "$PERSON1 :: how was the day $PERSON1, when did you came to london"

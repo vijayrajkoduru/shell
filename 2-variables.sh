10aad8c429f140372ed39ad3f4d1755265b56f92
Vijaykumar-Koduru <koduru.vijaykumar.7@gmail.com>
2025-07-16 10:41:50 +0530

    created

--- 2-variables.sh
+++ 2-variables.sh
@@ -5,4 +5,4 @@ PERSON2=Musk
 
 echo "$PERSON1:: hey $PERSON2. How is the life"
 echo "$PERSON2:: Hello $PERSON1, Nice to see you"
-echo "$PERSON1 :: how was the day $PERSON1, when did you came to london"
+echo "$PERSON1 :: how was the day $PERSON2, when did you came to london"

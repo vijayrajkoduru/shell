60f83a9a5c905839c18611681ab628a05cc7c5c8
Vijaykumar-Koduru <koduru.vijaykumar.7@gmail.com>
2025-07-16 11:04:50 +0530

    created

---
+++ 7-array.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+MOVIES=("PUSHP1", "PUSHA2", "Thandle", "kubera")
+
+echo "First movie= ${MOVIES[0]}"
+
+echo "Second movie= ${MOVIES[1]}"
+
+echo "Third movie= ${MOVIES[2]}"
+
+echo "Fourth movie= ${MOVIES[3]}"
+
+echo "All movies= ${MOVIES[@]}"
